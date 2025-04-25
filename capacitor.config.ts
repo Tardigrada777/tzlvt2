758d2cc199114a40e679a6f05075be544ba98e0f
Vladimir Myltcev <ntwrite@gmail.com>
2025-04-25 19:04:30 +0200

    setup capacitor and android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'tzlvt.vmyltsev.com',
+  appName: 'tzlvt2',
+  webDir: 'dist/tzlvt/browser',
+};
+
+export default config;
